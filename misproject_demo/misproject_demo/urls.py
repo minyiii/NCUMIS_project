2ab59c17969a4ba08d260e4b0c78b1a87e53ea5d
Kaiser <a1246369@outlook.com>
2020-05-28 21:11:17 +0800

    heyhey

--- misproject_demo/misproject_demo/urls.py
+++ misproject_demo/misproject_demo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#hey you
 urlpatterns = [
     path('welcome/', include('welcome.urls')),
     path('admin/', admin.site.urls),
