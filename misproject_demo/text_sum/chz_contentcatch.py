a2efe69299d34845044c5161da10816d3357e146
rena <ches40422@gmail.com>
2020-07-22 21:02:47 +0800

    增加程式碼

--- misproject_demo/text_sum/chz_contentcatch.py
+++ misproject_demo/text_sum/chz_contentcatch.py
@@ -23,3 +23,28 @@ def predo_content(lines):
 content = predo_content(lines)
 print(content)
 
+# 判斷哪些是h1 h2 h3 或其他
+# 判斷句首#數量來做分類
+def catch_label(content):
+  
+  h1 = ['h1']
+  h2 = ['h2']
+  h3 = ['h3']
+  p = ['p']
+
+  for i in range(len(content)):
+    num = lines[i].count("#") # 計算'#'數量
+    
+    # 判斷為哪類
+    if num == 1:
+      h1.append(content[i])
+    elif num == 2:
+      h2.append(content[i])
+    elif num == 3:
+      h3.append(content[i]) 
+    else:
+      p.append(content[i])
+ 
+  return h1, h2, h3, p
+
+catch_label(content)
