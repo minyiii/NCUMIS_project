890ee99eea34a8d0ee29e78b87c1d1f716421124
Kaiser <a1246369@outlook.com>
2020-08-18 11:56:24 +0800

    fix pylint error

--- authenticate_app/models.py
+++ authenticate_app/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.conf import settings
+#from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
 
 class jsonContent(models.Model):
     #author = models.ForeignKey('User', on_delete=models.CASCADE)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     upload = models.FileField(upload_to='uploads/')
-    content = models.CharField(max_length=1000)
+    content = models.CharField(max_length=1000, blank=True, null=True)
+    #content = JSONField(default=None)
+    # https://miny.app/u012111465
+    # https://www.cnblogs.com/huchong/p/7894860.html
+
 
     def __str__(self):
         return self.title
+
+
+
--- authenticate_app/views.py
+++ authenticate_app/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from . import models
+from django.conf import settings
+
+
 # Create your views here.
 def loginPage(request):
     if request.method == "POST":
@@ -25,16 +29,25 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request,"User created successfully")
-            return redirect('login')
+            return redirect('/login')
         else:
             messages.error(request,"There was an error")
             
     return render(request,"signup.html",{'form':form})
 
 def home(request):
-    user= request.user
-    return render(request,'welcome.html',{'user':user})
+    if request.user.is_authenticated:
+        user = request.user
+        
+        try:
+            user = settings.AUTH_USER_MODEL.object.get(user=user)
+            jsonContents = models.jsonContent.object.filter(author=user)
+        except:
+            pass
+
+    return render(request, 'welcome.html', locals())
+    #{'user':user}
 
 def logoutPage(request):
     logout(request)
-    return redirect ("login")
+    return redirect ("/login")
