3093a22a9d415a588085f983e2bf6d003d520ec4
Cindy <dazaiosamu67@g.ncu.edu.tw>
2020-08-20 17:46:11 +0800

    在install md 檔案之前的暫存檔

---
+++ vue-frontend/src/views/Notes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="notes">
+    <VNBar />
+  </div>
+</template>
+
+<script>
+import VNBar from "@/components/Navbar-Top-New/index.vue";
+export default {
+  name: "notes",
+  components: {
+    VNBar,
+  },
+};
+</script>
+
+<style scoped>
+.notes {
+  width: 100%;
+  height: 95vh;
+  padding-top: 100px;
+}
+</style>
--- vue-frontend/src/views/Settings.vue
+++ vue-frontend/src/views/Settings.vue
@@ -2,28 +2,37 @@
   <div class="settings">
     <VNBar />
     <div class="row box p-4">
-      <div class="col-12 col-md-6">
+      <div class="col-12 col-md-6 l-section">
         <h2>Settings</h2>
-        <form action>
-          <div class="form-group">
-            <label for="name">Name :</label>
-            <input type="text" id="name" class="form-control" />
-          </div>
-          <div class="form-group">
-            <label for="password">Password :</label>
-            <input type="password" id="password" class="form-control" />
-          </div>
-          <input type="submit" value="儲存" class="btn btn-dark w-100" />
-        </form>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="mt-4">
+          <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+            <b-form-input id="input-1" v-model="form.name" required placeholder="Enter name"></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="Email:" label-for="input-2">
+            <b-form-input id="input-2" type="email" v-model="form.email" required></b-form-input>
+          </b-form-group>
+          <b-row class="mt-3 p-3">
+            <b-button type="submit" class="btn btn-dark w-50">Save</b-button>
+            <b-button type="reset" class="btn btn-dark w-50">Cancel</b-button>
+          </b-row>
+        </b-form>
       </div>
-      <div class="col-12 col-md-6 text-center">
-        <div id="imgzone">
-          <img v-bind:src="photo" class="img-thumbnail" />
-        </div>
-        <div class="btn-group">
-          <!-- <input type="reset" value="Reset Image" class="btn btn-dark"> -->
-          <input type="submit" value="Upload Image" class="btn btn-dark" />
-        </div>
+      <div class="col-12 col-md-6 text-center r-section">
+        <b-img v-bind:src="photo" v-bind="mainProps" />
+        <!-- <b-button type="submit" class="btn btn-dark" @change="UploadImages">Upload Images</b-button> -->
+        <!-- <input type="reset" value="Reset Image" class="btn btn-dark" /> -->
+        <b-form-file
+          v-model="file"
+          ref="file-input"
+          accept="image/jpeg, image/png"
+          placeholder="Upload Images"
+          drop-placeholder="Drop file here..."
+          @change="UploadImages"
+          class="mt-3"
+          style="overflow: hidden; width:350px"
+        ></b-form-file>
+        <b-button @click="resetFiles" style="width:350px">Reset Images</b-button>
       </div>
     </div>
   </div>
@@ -40,8 +49,49 @@ export default {
   },
   data() {
     return {
-      photo: "https://picsum.photos/300/300",
+      file: null,
+      photo:
+        "https://pbs.twimg.com/media/Efx05jfUwAAodUy?format=jpg&name=large",
+      mainProps: {
+        width: "350px",
+        height: "300px",
+      },
+      form: {
+        name: "",
+        email: "",
+      },
+      show: true,
     };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.name = "";
+      this.form.email = "";
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+    UploadImages(e) {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = (e) => {
+        this.photo = e.target.result;
+      };
+    },
+    resetFiles() {
+      this.photo =
+        "https://pbs.twimg.com/media/Efx05jfUwAAodUy?format=jpg&name=large";
+      this.$refs["file-input"].reset();
+    },
   },
 };
 </script>
@@ -57,12 +107,11 @@ export default {
   margin: auto;
   background: white;
   border: 1px solid #e2dada;
-  width: 70%;
-  height: 80%;
+  width: 80%;
+  height: 90%;
   position: relative;
   margin-top: 100px;
   text-align: left;
-  /* top: 100px; */
 }
 .box .btn {
   margin-top: 10px;
