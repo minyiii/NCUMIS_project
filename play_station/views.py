494c05d8e083d28d898d03e8b30fb78199d9b4d0
Kaiser <a1246369@outlook.com>
2020-08-27 12:43:40 +0800

    modify play_station model

--- play_station/views.py
+++ play_station/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    if request.user.is_authenticated:
+        user = request.user
+        
+        try:
+            title = models.jsonContent.object.filter(author=user)
+        except:
+            pass
+
+    return render(request, 'welcome.html', locals())
